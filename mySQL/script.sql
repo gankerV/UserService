452be82accc25e64d519f87277da1f96bf94a1e3
xanh <nvxanh75@gmail.com>
2025-06-18 11:49:57 +0700

    1

---
+++ mySQL/script.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS `user_db`;
+CREATE DATABASE `user_db`;
+USE `user_db`;
+
+CREATE TABLE `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL
+);
